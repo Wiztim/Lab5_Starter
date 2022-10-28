f6586490c452b340bd77ac77d227a784151580db
Tim Orlov <timothyorolv@gmail.com>
2022-10-27 23:06:21 -0700

    finished part 2

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,41 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+
 function init() {
-  // TODO
+  let asdf = new SpeechSynthesisUtterance("hello world");
+  //speechSynthesis.speak(asdf)
+}
+
+const synth = window.speechSynthesis;
+const select = document.getElementById("voice-select");
+var voiceList;
+synth.addEventListener('voiceschanged', () => {
+  voiceList = synth.getVoices();
+  for (let i = 0; i < voiceList.length; i++) {
+    let voiceEntry = voiceList[i].name + " (" + voiceList[i].lang + ")";
+    let selectEntry = document.createElement("option");
+    selectEntry.textContent = voiceEntry;
+    select.append(selectEntry);
+  }
+});
+
+const textArea = document.getElementById("text-to-speak");
+const button = document.querySelector("button");
+const image = document.querySelector("img");
+button.addEventListener('click', () => {
+  let ttsText = new SpeechSynthesisUtterance(textArea.value);
+  ttsText.voice = voiceList[select.selectedIndex - 1];
+  speechSynthesis.speak(ttsText);
+});
+
+setInterval(ttsSpeaking, 100);
+
+function ttsSpeaking() {
+  if (synth.speaking) {
+    image.src = "assets/images/smiling-open.png";
+  }
+  else {
+    image.src = "assets/images/smiling.png";
+  }
 }
